a69b2cd387f83f57e5ae6cb91a04d550f7b685ce
vitaliy <vitaliynif@gmail.com>
2022-12-18 14:49:07 +0700

    first component created

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,7 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
     <div class="container">
-      <section>
-        <div class="flex">
-          <div class="max-w-xs">
-            <label for="wallet" class="block text-sm font-medium text-gray-700"
-              >Тикер</label
-            >
-            <div class="mt-1 relative rounded-md shadow-md">
-              <input
-                v-model="ticker"
-                @keydown.enter="add"
-                type="text"
-                name="wallet"
-                id="wallet"
-                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                placeholder="Например DOGE"
-              />
-            </div>
-            <div
-              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
-            >
-              <span
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
-              >
-                BTC
-              </span>
-              <span
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
-              >
-                DOGE
-              </span>
-              <span
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
-              >
-                BCH
-              </span>
-              <span
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
-              >
-                CHD
-              </span>
-            </div>
-            <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
-          </div>
-        </div>
-        <button
-          @click="add"
-          type="button"
-          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-        >
-          <!-- Heroicon name: solid/mail -->
-          <svg
-            class="-ml-0.5 mr-2 h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="#ffffff"
-          >
-            <path
-              d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-            ></path>
-          </svg>
-          Добавить
-        </button>
-      </section>
-
+      <add-ticker @add-ticker="add" :disabled="tooManyTickers" />
       <button
         @click="page = page - 1"
         v-if="page > 1"
@@ -132,7 +67,11 @@
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
           {{ selectedTicker.name }} - USD
         </h3>
-        <div class="flex items-end border-gray-600 border-b border-l h-64">
+        <div
+          class="flex items-end border-gray-600 border-b border-l h-64"
+          ref="graph"
+          id="asd"
+        >
           <div
             v-for="(bar, idx) in normalizedGraph"
             :key="idx"
@@ -174,22 +113,27 @@
 
 <script>
 import { subscribeToTicker, unsubscribeFromTicker } from "./api";
+import AddTicker from "@/components/AddTicker.vue";
 
 export default {
   name: "App",
   data() {
     return {
-      ticker: "",
       tickers: [],
       selectedTicker: null,
       apiKey: "",
       graph: [],
       filter: "",
       page: 1,
+      maxGraphElements: 1,
     };
   },
+  components: { AddTicker },
 
   computed: {
+    tooManyTickers() {
+      return this.tickers.length > 10;
+    },
     startIndex() {
       return (this.page - 1) * 6;
     },
@@ -260,13 +204,35 @@ export default {
     }, 5000);
   },
 
+  mounted() {
+    window.addEventListener("resize", this.calculateMaxGraphElements);
+  },
+
+  beforeUnmount() {
+    window.removeEventListener("resize", this.calculateMaxGraphElements);
+  },
+
   methods: {
+    calculateMaxGraphElements() {
+      if (!this.$refs.graph) {
+        return;
+      }
+      this.maxGraphElements = Math.trunc(this.$refs.graph.clientWidth / 38);
+    },
     updateTicker(tickerName, price) {
       this.tickers
         .filter((t) => t.name === tickerName)
         .forEach((t) => {
           if (t === this.selectedTicker) {
             this.graph.push(price);
+            if (this.graph.length > this.maxGraphElements) {
+              this.graph = this.graph.slice(
+                this.graph.length - this.maxGraphElements,
+                this.graph.length
+              );
+              // TODO: when graph resize div is too small
+              // TODO: w-10 in graph value = 38 (ref/ hardcode)
+            }
           }
           t.price = price;
         });
@@ -287,13 +253,12 @@ export default {
       // });
     },
 
-    add() {
-      const currentTicker = { name: this.ticker, price: "-" };
+    add(ticker) {
+      const currentTicker = { name: ticker, price: "-" };
       this.tickers = [...this.tickers, currentTicker];
 
       this.updateTickers(currentTicker.name);
 
-      this.ticker = "";
       this.filter = "";
       subscribeToTicker(currentTicker.name, (newPrice) =>
         this.updateTicker(currentTicker.name, newPrice)
@@ -325,6 +290,7 @@ export default {
 
     selectedTicker() {
       this.graph = [];
+      this.$nextTick().then(this.calculateMaxGraphElements);
     },
 
     filter() {
